ffca5c601dd97862f8fa72afe46e33b85f2dc75e
Roman <roman.f.grusha@gmail.com>
2022-06-08 13:51:30 +0300

    ContactsItem added

---
+++ src/components/ContactsList/ContactsItem/ContactsItem.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeContact } from '../../../redux/contactsSlice';
+import { Button, Item, Text } from './ContactsItem.styled';
+import { VscClose } from 'react-icons/vsc';
+
+export const ContactsItem = ({ id, name, number }) => {
+  const dispatch = useDispatch();
+  const handleDeleteContact = currentId => {
+    dispatch(removeContact(currentId));
+  };
+
+  return (
+    <Item>
+      <Text>{name}</Text>
+      <Text>{number}</Text>
+      <Button
+        type="button"
+        onClick={() => {
+          handleDeleteContact(id);
+        }}
+      >
+        <VscClose size={20} />
+      </Button>
+    </Item>
+  );
+};
+
+ContactsItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+};
